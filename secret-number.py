25ba85835a0b73e4a02bdca760d879493f9a2484
Dea <dea.chaudhery@gmail.com>
2019-06-25 23:23:09 -0400

    weekday function

--- secret-number.py
+++
@@ -1,9 +0,0 @@
-def sleep_in(weekday, vacation):
-    if weekday == False and vacation == False:
-       return True
-      elif:
-        weekday == False and vacation == False:
-        return False
-     else
-        weekday == False and vacation == True:
-        return True
