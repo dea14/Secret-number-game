db68edf019ce0d946117f02d18ed634aee757f24
Dea <dea.chaudhery@gmail.com>
2019-07-03 22:37:49 -0400

    codingbat

--- secret-number.py
+++ secret-number.py
@@ -1,29 +1,8 @@
-def cigar_party(cigars, is_weekend):
-  if cigars >= 40 and cigars <= 60 or is_weekend == True:
-    return True 
-  elif cigars >= 40 or is_weekend == True:
-    return True
-  else:
-    return False
-print((cigar_party(20, True)))
-
-
-
-def date_fashion(you, date):
-    if you >= 8:
-        if date > 2:
-            return 2
-        else:
-            return 0
-    elif date >= 8:
-        if you > 2:
-            return 2
-        else:
-            return 0
-    elif you <= 2 and you < 8 and date <= 2 and date < 0:
-        return 0
+def make_bricks(small, big, goal):
+    if goal%5 == big:
+       return True
     else:
-        return 1
-
-print(date_fashion(3,7))
-
+        if goal%5 >= small:
+            return True
+        else:
+            return False
