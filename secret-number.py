09672038e9e95eb40be2c701800aa39a9153fa6f
Dea <dea.chaudhery@gmail.com>
2019-07-07 00:48:18 -0400

    brick problem

--- secret-number.py
+++ secret-number.py
@@ -1,25 +1,19 @@
-'''def make_bricks(small, big, goal):
+def make_bricks(small, big, goal):
     if 5 * big == goal:
         return True
-    elif 1 * small == goal:
+    elif 1 * small >= goal:
         return True
-    elif (5 * big) > goal and ((5 * big) - goal) >= small:
+    elif small < goal and (goal-small) % 5 == 0 and (goal-small)/5 <= big:
+        return True
+    elif (5 * big) < goal and (goal - (5 * big)) <= small and (goal - ((5 * big))) <= small:
         return True
     else:
-        (5 * big) < goal and (goal - (5 * big)) >= small
-        return True
+        return False
 
 
-print(make_bricks(3,1,9))'''
+print(make_bricks(3,1,9))
 
 
-def lone_sum(a, b, c):
-    if a != b and a != c and b != c:
-        return a + b + c
-    elif a != b and a=c:
-        return b
-    elif b != a and b=c:
-        return a
 
 
 
