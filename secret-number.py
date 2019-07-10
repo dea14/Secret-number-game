3414b57289ceef8ef8691e10f984b3821977b098
Dea <dea.chaudhery@gmail.com>
2019-07-09 23:55:07 -0400

    makeChocoProblem

--- secret-number.py
+++ secret-number.py
@@ -1,19 +1,3 @@
-def make_bricks(small, big, goal):
-    if 5 * big == goal:
-        return True
-    elif 1 * small >= goal:
-        return True
-    elif small < goal and (goal-small) % 5 == 0 and (goal-small)/5 <= big:
-        return True
-    elif (5 * big) < goal and (goal - (5 * big)) <= small and (goal - ((5 * big))) <= small:
-        return True
-    else:
-        return False
-
-
-print(make_bricks(4,1,9))
-
-
 
 
 
