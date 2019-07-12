087f2c2295e077be2a9b78e438af0811797bd34e
Dea <dea.chaudhery@gmail.com>
2019-07-11 20:43:42 -0400

    number

--- secret-number.py
+++ secret-number.py
@@ -1,7 +1,17 @@
-if (5*big)+(1*small)==goal:
+def make_chocolate(small, big, goal):
+  if (5*big)+(1*small)==goal:
     return small
-elif (5*big) == goal
-    
+  elif (5*big) == goal:
+    return 0
+  elif (5*big) > goal and (goal-(5*big))%5 == 0:
+    return 0
+  elif (5*big) < goal and (1*small) == goal:
+    return small
+  elif (5*big) < goal and (goal-(5*big)) < small:
+    return small - (goal-(5*big)
+  else:
+    return -1
+
 
 
 
